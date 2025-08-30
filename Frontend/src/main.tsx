fdeb067b0c60df7be385147d7128f11fec8a074a
Rohit-1717 <rohitvishwakarma1717@gmail.com>
2025-08-31 03:20:37 +0530

    Fixed Type Error

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -5,14 +5,19 @@ import App from "./App.tsx";
 import { ThemeProvider } from "./components/theme/theme-provider.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "sonner";
+import type { ToasterProps } from "sonner";
 
+const toasterOptions: ToasterProps = {
+  richColors: true,
+  position: "top-right",
+};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <BrowserRouter>
         <App />
-        <Toaster richColors position="top-right"/>
+        <Toaster {...toasterOptions} />
       </BrowserRouter>
     </ThemeProvider>
   </StrictMode>
